838d93f9a9c79db05558e1a44c9448bbed220030
ehddn6570 <ehddn6570@gmail.com>
2025-11-15 06:02:21 +0900

    1

--- eatwise/eatwise/src/main/java/com/project/eatwise/EatwiseApplication.java
+++ eatwise/eatwise/src/main/java/com/project/eatwise/EatwiseApplication.java
@@ -2,10 +2,8 @@ package com.project.eatwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.project.eatwise", "domain", "global"})
 public class EatwiseApplication {
 
 	public static void main(String[] args) {
