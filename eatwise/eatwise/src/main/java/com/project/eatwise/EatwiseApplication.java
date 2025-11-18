c5d9c9ded8ef94622f775dbc3777b700caf0e7a4
ehddn6570 <ehddn6570@gmail.com>
2025-11-18 20:06:00 +0900

    1

--- eatwise/eatwise/src/main/java/com/project/eatwise/EatwiseApplication.java
+++ eatwise/eatwise/src/main/java/com/project/eatwise/EatwiseApplication.java
@@ -2,8 +2,12 @@ package com.project.eatwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.project.eatwise", "global", "domain"})
+@EnableJpaRepositories(basePackages = {"domain"})
+@EntityScan(basePackages = {"domain"})
 public class EatwiseApplication {
 
 	public static void main(String[] args) {
